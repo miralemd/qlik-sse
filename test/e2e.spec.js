8ec5dd4e51927eae9a5911f03f714f8a88321179
Miralem Drek <mm85dk@gmail.com>
2019-10-13 12:15:06 +0200

    fix: catch script errors

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -39,7 +39,17 @@ describe('e2e', () => {
     s = server({
       identifier: 'xxx',
       version: '0.1.0',
-      allowScript: true,
+      allowScript: {
+        scriptEval: true,
+        scriptEvalStr: true,
+        scriptAggr: true,
+        scriptAggrStr: true,
+        // ----
+        scriptEvalEx: true,
+        scriptEvalExStr: false,
+        scriptAggrEx: true,
+        scriptAggrExStr: true,
+      },
       logLevel: 0,
     });
 
@@ -185,7 +195,7 @@ describe('e2e', () => {
     });
   });
 
-  describe('executeScript', () => {
+  describe('evaluateScript', () => {
     it('should duplicate numbers', (done) => {
       const sh = new sse.ScriptRequestHeader({
         script: 'return args[0] * 2',
@@ -214,11 +224,160 @@ describe('e2e', () => {
 
       const e = c.evaluateScript(metadata);
 
-      e.on('data', ({ rows }) => {
-        expect(rows).to.eql([{ duals: [{ numData: 12, strData: '' }] }]);
+      let data = {};
+      const assert = () => {
+        expect(data.rows).to.eql([{ duals: [{ numData: 12, strData: '' }] }]);
+        done();
+      };
+
+      e.on('data', (d) => { data = d; });
+
+      e.on('end', assert);
+
+      e.write(b);
+      e.end();
+    });
+
+    it('should duplicate numbers aggr', (done) => {
+      const sh = new sse.ScriptRequestHeader({
+        script: 'return args[0] * 2',
+        functionType: sse.FunctionType.AGGREGATION,
+        returnType: sse.DataType.NUMERIC,
+        params: [{ dataType: sse.DataType.NUMERIC, name: 'f' }],
+      }).encodeNB();
+
+      const ch = new sse.CommonRequestHeader({
+        appId: 'aa',
+        userId: 'uu',
+        cardinality: 55,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-scriptrequestheader-bin', sh);
+      metadata.set('qlik-commonrequestheader-bin', ch);
+
+      const b = new sse.BundledRows({
+        rows: [{
+          duals: [{
+            numData: 6,
+          }],
+        }],
       });
 
-      e.on('end', done);
+      const e = c.evaluateScript(metadata);
+
+      let data = {};
+      const assert = () => {
+        expect(data.rows).to.eql([{ duals: [{ numData: 12, strData: '' }] }]);
+        done();
+      };
+
+      e.on('data', (d) => { data = d; });
+
+      e.on('end', assert);
+
+      e.write(b);
+      e.end();
+    });
+
+    it('should catch script parsing error', (done) => {
+      const sh = new sse.ScriptRequestHeader({
+        script: 'blabla invalid javascript',
+        functionType: sse.FunctionType.SCALAR,
+        returnType: sse.DataType.NUMERIC,
+        params: [{ dataType: sse.DataType.NUMERIC, name: 'f' }],
+      }).encodeNB();
+
+      const ch = new sse.CommonRequestHeader({
+        appId: 'aa',
+        userId: 'uu',
+        cardinality: 55,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-scriptrequestheader-bin', sh);
+      metadata.set('qlik-commonrequestheader-bin', ch);
+
+      const e = c.evaluateScript(metadata);
+
+      let err = {};
+      const assert = () => {
+        expect(err.code).to.eql(grpc.status.INVALID_ARGUMENT);
+        done();
+      };
+
+      e.on('data', () => {});
+      e.on('error', (er) => { err = er; assert(); });
+      e.end();
+    });
+
+    it('should not allow scriptEvalExStr', (done) => {
+      const sh = new sse.ScriptRequestHeader({
+        script: 'return 0',
+        functionType: sse.FunctionType.SCALAR,
+        returnType: sse.DataType.STRING,
+        params: [{ dataType: sse.DataType.DUAL, name: 'f' }],
+      }).encodeNB();
+
+      const ch = new sse.CommonRequestHeader({
+        appId: 'aa',
+        userId: 'uu',
+        cardinality: 55,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-scriptrequestheader-bin', sh);
+      metadata.set('qlik-commonrequestheader-bin', ch);
+
+      const e = c.evaluateScript(metadata);
+
+      let err = {};
+      const assert = () => {
+        expect(err.code).to.eql(grpc.status.PERMISSION_DENIED);
+        done();
+      };
+
+      e.on('data', () => {});
+      e.on('error', (er) => { err = er; assert(); });
+      e.end();
+    });
+
+    it('should catch script execution error', (done) => {
+      const sh = new sse.ScriptRequestHeader({
+        script: 'return args.foo.nope',
+        functionType: sse.FunctionType.SCALAR,
+        returnType: sse.DataType.NUMERIC,
+        params: [{ dataType: sse.DataType.NUMERIC, name: 'f' }],
+      }).encodeNB();
+
+      const ch = new sse.CommonRequestHeader({
+        appId: 'aa',
+        userId: 'uu',
+        cardinality: 55,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-scriptrequestheader-bin', sh);
+      metadata.set('qlik-commonrequestheader-bin', ch);
+
+      const b = new sse.BundledRows({
+        rows: [{
+          duals: [{
+            numData: 6,
+          }],
+        }],
+      });
+
+      const e = c.evaluateScript(metadata);
+
+      let data = {};
+      const assert = () => {
+        expect(data.rows).to.eql([{ duals: [{ numData: 0, strData: '' }] }]);
+        done();
+      };
+
+      e.on('data', (d) => { data = d; });
+      e.on('end', assert);
 
       e.write(b);
       e.end();
