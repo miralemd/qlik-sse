0731dd99171f9ef124babfa49afc68bec2b1f2b0
Miralem Drek <mm85dk@gmail.com>
2019-10-13 10:45:48 +0200

    test: basic e2e grpc test

---
+++ test/e2e.spec.js
@@ -0,0 +1,159 @@
+const grpc = require('grpc');
+const { server, sse } = require('../lib/index');
+
+function duplicate(request) {
+  request.on('data', (bundle) => {
+    const rows = [];
+    bundle.rows.forEach((row) => {
+      row.duals.forEach((dual) => {
+        rows.push({
+          duals: [{ numData: dual.numData * 2 }],
+        });
+      });
+    });
+    request.write({ rows });
+  });
+}
+
+describe('e2e', () => {
+  let s;
+  let c;
+  before(() => {
+    s = server({
+      identifier: 'xxx',
+      version: '0.1.0',
+      allowScript: true,
+      logLevel: 0,
+    });
+
+    s.addFunction(duplicate, { // eslint-disable-line
+      functionType: sse.FunctionType.SCALAR,
+      returnType: sse.DataType.NUMERIC,
+      params: [{
+        name: 'first',
+        dataType: sse.DataType.NUMERIC,
+      }],
+    });
+
+    s.start({
+      port: 5001,
+    });
+
+    c = new sse.Connector('0.0.0.0:5001', grpc.credentials.createInsecure());
+  });
+
+  after(() => {
+    s.close();
+  });
+
+  describe('getCapabilities', () => {
+    it('should return a Capabilities object', (done) => {
+      c.getCapabilities(new sse.Empty(), (x, cap) => {
+        const type = () => new sse.Capabilities(cap);
+        expect(type).to.not.throw();
+        expect(cap).to.eql({
+          allowScript: true,
+          functions: [{
+            name: 'duplicate',
+            functionType: 'SCALAR',
+            returnType: 'NUMERIC',
+            params: [{ dataType: 'NUMERIC', name: 'first' }],
+            functionId: 1001,
+          }],
+          pluginIdentifier: 'xxx',
+          pluginVersion: '0.1.0',
+        });
+        done();
+      });
+    });
+  });
+
+  describe('executeFunction', () => {
+    it('should duplicate numbers', (done) => {
+      const fmh = new sse.FunctionRequestHeader({
+        functionId: 1001,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-functionrequestheader-bin', fmh);
+
+      const b = new sse.BundledRows({
+        rows: [{
+          duals: [{
+            numData: 7,
+          }],
+        }],
+      });
+
+      const e = c.executeFunction(metadata);
+
+      e.on('data', ({ rows }) => {
+        expect(rows).to.eql([{ duals: [{ numData: 14, strData: '' }] }]);
+      });
+
+      e.on('end', done);
+
+      e.write(b);
+      e.end();
+    });
+
+    it('should emit UMIMPLEMENTED error when method is not found', (done) => {
+      const fmh = new sse.FunctionRequestHeader({
+        functionId: 99,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-functionrequestheader-bin', fmh);
+
+      const e = c.executeFunction(metadata);
+
+      e.on('data', () => {});
+      e.on('error', (err) => {
+        expect(err.code).to.equal(grpc.status.UNIMPLEMENTED);
+        expect(err.details).to.equal('The method is not implemented.');
+        done();
+      });
+      e.end();
+    });
+  });
+
+  describe('executeScript', () => {
+    it('should duplicate numbers', (done) => {
+      const sh = new sse.ScriptRequestHeader({
+        script: 'return args[0] * 2',
+        functionType: sse.FunctionType.SCALAR,
+        returnType: sse.DataType.NUMERIC,
+        params: [{ dataType: sse.DataType.NUMERIC, name: 'f' }],
+      }).encodeNB();
+
+      const ch = new sse.CommonRequestHeader({
+        appId: 'aa',
+        userId: 'uu',
+        cardinality: 55,
+      }).encodeNB();
+
+      const metadata = new grpc.Metadata();
+      metadata.set('qlik-scriptrequestheader-bin', sh);
+      metadata.set('qlik-commonrequestheader-bin', ch);
+
+      const b = new sse.BundledRows({
+        rows: [{
+          duals: [{
+            numData: 6,
+          }],
+        }],
+      });
+
+      const e = c.evaluateScript(metadata);
+
+      e.on('data', ({ rows }) => {
+        expect(rows).to.eql([{ duals: [{ numData: 12, strData: '' }] }]);
+      });
+
+      e.on('end', done);
+
+      e.write(b);
+      e.end();
+    });
+  });
+});
